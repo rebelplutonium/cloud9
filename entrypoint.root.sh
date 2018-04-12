df6d83f24b12278e49ea4055d2d41cb7ff3a89ae
Emory Merryman <emory.merryman@gmail.com>
2018-04-12 11:19:19 +0000

    add post scripts

--- entrypoint.root.sh
+++ entrypoint.root.sh
@@ -16,9 +16,13 @@ fi &&
     then
         sh /opt/cloud9/extension/init.root.sh
     fi &&
-    if [ ! -d ${WORKSPACE_DIR} ]
+    if [ ! -d ${CLOUD9_WORKSPACE} ]
     then
-        mkdir ${WORKSPACE_DIR} &&
-            chown -R user:user ${WORKSPACE_DIR}
+        mkdir ${CLOUD9_WORKSPACE} &&
+            chown -R user:user ${CLOUD9_WORKSPACE}
+    fi &&
+    if [ -f /opt/cloud9/extension/post.root.sh ]
+    then
+        nohup sh /opt/cloud9/extension/post.root.sh &
     fi &&
     su -c "sh /opt/cloud9/scripts/entrypoint.user.sh" user
