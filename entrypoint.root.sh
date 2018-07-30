3c86833777ff4e169d7aefc3d6e7daf95f72465d
Emory Merryman <emory.merryman@gmail.com>
2018-07-30 19:33:15 +0000

    add sudo, completion

--- entrypoint.root.sh
+++ entrypoint.root.sh
@@ -12,10 +12,9 @@ export PATH=/opt/cloud9/sbin:${PATH} &&
     then
         sh /opt/cloud9/extension/parse.sh "${@}"
     fi &&
-    shift ${#} &&
     if [ -f /opt/cloud9/extension/init.root.sh ]
     then
-        sh /opt/cloud9/extension/init.root.sh
+        sh /opt/cloud9/extension/init.root.sh "${@}"
     fi &&
     if [ ! -d ${CLOUD9_WORKSPACE} ]
     then
@@ -26,4 +25,4 @@ export PATH=/opt/cloud9/sbin:${PATH} &&
     then
         nohup sh /opt/cloud9/extension/post.root.sh &
     fi &&
-    su -c "sh /opt/cloud9/scripts/entrypoint.user.sh" user
+    su -c "sh /opt/cloud9/scripts/entrypoint.user.sh \"${@}\"" user
