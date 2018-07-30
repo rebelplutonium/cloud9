12c0986a34713566274720d1af45e6517bfd6703
Emory Merryman <emory.merryman@gmail.com>
2018-07-30 19:33:15 +0000

    add sudo, completion

--- entrypoint.user.sh
+++ entrypoint.user.sh
@@ -16,5 +16,9 @@ export PATH=/opt/cloud9/bin:${PATH} &&
     then
         nohup sh /opt/cloud9/extension/post.user.sh &
     fi &&
+    for FILE in $(ls -1 /opt/cloud9/completion)
+    do
+        source /opt/cloud9/completion/${FILE}
+    done &&
     PROJECT_NAME="${PROJECT_NAME}" node /opt/cloud9/c9sdk/server.js --listen 0.0.0.0 -w ${CLOUD9_WORKSPACE} -p ${CLOUD9_PORT} &&
     shift ${#}
